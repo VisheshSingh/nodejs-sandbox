f43e26ad85cb7822c6e7ef632b51cd257061b06e
Vishesh <visheshsingh16@gmail.com>
2017-11-04 19:13:15 -0400

    Basic Routing

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ var fs = require('fs');
 
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+        myReadStream.pipe(res);
+    } else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var myReadStream = fs.createReadStream(__dirname + '/contact.html', 'utf8');
+        myReadStream.pipe(res);
+    } else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name:'yoshi', age: 25}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        var myReadStream = fs.createReadStream(__dirname + '/404.html', 'utf8');
+        myReadStream.pipe(res);
+    }
     
-    myReadStream.pipe(res);
+    //res.end('howdy gang!');
 
 });
 
