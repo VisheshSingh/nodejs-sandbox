3f5e16c7491e0d91fe48fe9a9987dacc041d302b
Vishesh <visheshsingh16@gmail.com>
2017-11-04 01:01:33 -0400

    Function expressions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('Howdy Ninjas!');
+
+//Function Definition
+function sayHi() {
+    console.log('Hi there!');
+}
+
+sayHi();
+//Function expressions
+var sayHello = function() {
+    console.log('Hello there!');
+}
+
+sayHello();
+
+//Callback function
+function callback(func) {
+    func();
+}
+
+callback(sayHi);
