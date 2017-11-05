3b173111f0bbc89d55ff11aa60a8e718e55f9af3
Vishesh <visheshsingh16@gmail.com>
2017-11-05 11:49:46 -0500

    Express route Params

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.get('/', function(req,res){
 app.get('/contact', function(req,res){
     res.send('this is the contact page');
 })
+
+app.get('/profile/:id', function(req,res){
+    res.send('You requested:' + req.params.id);
+})
+
 app.listen(3001);
 console.log('Now listening to port:3001');
